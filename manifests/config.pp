a4a7f639f29aaf537a49322061abc0b79e81a345
foster82 <ilovemrsbaum@yahoo.co.uk>
2018-10-12 02:37:19 +0100

    Update config.pp

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,7 +1,7 @@
 class nagios::config{
 file { '/usr/local/nagios/etc/servers/host.cfg':
    ensure       => present,
-   mode         => '0644',
+   mode         => '0755',
    owner        => 'root',
    group        => 'nagios',  
    source       => 'puppet:///environments/testing/modules/nagios/files/host.cfg',
