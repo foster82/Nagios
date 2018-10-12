ef66acb610fab41dc31aa03c6917162a84740c65
foster82 <ilovemrsbaum@yahoo.co.uk>
2018-10-12 03:13:04 +0100

    Update config.pp

--- manifests/config.pp
+++ manifests/config.pp
@@ -10,5 +10,28 @@ file { 'host.cfg':
                      
     
   }
+  file { 'vm01.cfg':
+   ensure       => present,
+   mode         => '0755',
+   owner        => 'root',
+   group        => 'nagios',  
+   source       => "puppet:///modules/nagios/vm01.cfg",
+   path         => "/usr/local/nagios/etc/servers/vm01.cfg",
+  
+                     
+    
+  }  
+  
+  file { 'vm02.cfg':
+   ensure       => present,
+   mode         => '0755',
+   owner        => 'root',
+   group        => 'nagios',  
+   source       => "puppet:///modules/nagios/vm02.cfg",
+   path         => "/usr/local/nagios/etc/servers/vm02.cfg",
+  
+                     
+    
+  }
   
   }
