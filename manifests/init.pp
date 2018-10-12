f6bd39a5672ab22cdedb879a6532791dae47f46f
foster82 <ilovemrsbaum@yahoo.co.uk>
2018-10-12 01:06:10 +0100

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,11 @@
 
-file { '/usr/local/nagios/etc/servers/':
-    path         => '/usr/local/nagios/etc/servers/,
-    ensure       => present,
-    owner => "root",
-    group => "nagios",
-    require      => File['/usr/local/nagios/etc/servers/'],
-    source       => 'puppet:///modules/files/host.cfg'
+file { 'host.cfg':
+   ensure       => present,
+   path         => '/usr/local/nagios/etc/servers/,
+   owner        => "root",
+   group        => "nagios",
+   require      => File['/usr/local/nagios/etc/servers/'],
+   source       => 'puppet:///modules/files/host.cfg',
                      
     
   }
