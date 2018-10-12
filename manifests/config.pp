f54f8db2734bd401090e1a4d784dedcaeee5784a
foster82 <ilovemrsbaum@yahoo.co.uk>
2018-10-12 02:34:07 +0100

    Update config.pp

--- manifests/config.pp
+++ manifests/config.pp
@@ -4,7 +4,7 @@ file { '/usr/local/nagios/etc/servers/host.cfg':
    mode         => '0644',
    owner        => 'root',
    group        => 'nagios',  
-   source       => 'puppet:///modules/nagios/files/host.cfg',
+   source       => 'puppet:///environments/testing/modules/nagios/files/host.cfg',
   
                      
     
