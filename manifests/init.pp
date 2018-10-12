54224a62342fded7fb9326a197b7fa220c260113
foster82 <ilovemrsbaum@yahoo.co.uk>
2018-10-12 01:02:27 +0100

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,13 @@
 
 file { '/usr/local/nagios/etc/servers/':
     path         => '/usr/local/nagios/etc/servers/,
-    ensure       => directory,
+    ensure       => present,
     owner => "root",
     group => "nagios",
     require      => File['/usr/local/nagios/etc/servers/'],
     source       => ['puppet:///modules/files/host.cfg',
                      'puppet:///modules/files/vm01.cfg'
                      'puppet:///modules/files/vmo2.cfg],
-    sourceselect => all,
+    
   }
   
